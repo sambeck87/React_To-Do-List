762f718fb4e7c5862a8048a3573851abb16a23ef
sambeck87 <sandro_mipb@hotmail.com>
2023-01-17 13:33:54 -0600

    Delete all unnecesary files Solve warning issues

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import TodoContainer from './functionBased/components/TodoContainer';
 import './functionBased/App.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <TodoContainer />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
